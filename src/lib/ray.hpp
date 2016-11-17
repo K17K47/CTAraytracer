62d87dbfd137639577527dab51301c4d167b46a8
K17K47 <k17k47@gmail.com>
2016-11-17 17:23:06 -0200

    Added licensing disclaimer

---
+++ src/lib/ray.hpp
@@ -0,0 +1,34 @@
+/*
+*    CTARaytracer
+*    Copyright (C) 2016  Lucas Lopes Costa <lucas_lc.hf@hotmail.com>
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 3 of the License, or (at your option) any later version.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef RAY_HPP_
+#define RAY_HPP_
+
+#include"aux/math.hpp"
+#include"aux/octree.hpp"
+
+namespace CTAR{
+
+   class ray{
+      Vector3 origin;
+      Vector3 dir;
+   };
+};
+
+#endif
