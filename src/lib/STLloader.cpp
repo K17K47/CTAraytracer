da02178e6ab885b604d7b0097f6970d843395ac8
K17K47 <k17k47@gmail.com>
2016-11-23 11:16:59 -0200

    Changed error code, reformatted code

--- src/lib/STLloader.cpp
+++ src/lib/STLloader.cpp
@@ -30,57 +30,60 @@ int loadSTLModel(Model **model, std::string path){
       int length = file.tellg();
       file.seekg(0, file.beg);
 
-      if(length < sizeof(STL_Header)){
-         std::cout<<"Arquivo é menor que cabeçalho\n";
-         return 0;
-      }
+      if(length < sizeof(STL_Header)) return 1;
+
       STL_Header fileHeader;
 
       file.read(reinterpret_cast<char*>(&fileHeader), sizeof(STL_Header));
-      if(!file){
-         std::cout<<"Leitura do cabeçalho falhou\n";
-         return 0;
-      }
+      if(!file) return 1;
 
-      std::cout<<"Número de triângulos:"<<fileHeader.triangleCount<<"\n";
+      if(fileHeader.triangleCount*sizeof(STL_Triangle)+sizeof(STL_Header)
+         != length) return 1;
 
-      if(fileHeader.triangleCount*sizeof(STL_Triangle)+sizeof(STL_Header) != length){
-         std::cout<<"Arquivo tem contagem de triângulos errada\n";
-         return 0;
-      }
       *model = new Model;
 
       STL_Triangle *buffer = new STL_Triangle[fileHeader.triangleCount];
 
-      file.read(reinterpret_cast<char*>(buffer), fileHeader.triangleCount*sizeof(STL_Triangle));
-      if(!file){
-         std::cout<<"Leitura dos triângulos falhou\n";
-         return 0;
-      }
+      file.read(reinterpret_cast<char*>(buffer),
+                fileHeader.triangleCount*sizeof(STL_Triangle));
+      if(!file) return 1;
 
       Triangle tri;
 
       Vector3 xCoords, yCoords, zCoords;
 
       for(int i=0; i<fileHeader.triangleCount; i++){
-            (*model)->vtx.push_back(Vector3(buffer[i].vtx1[0],buffer[i].vtx1[1],buffer[i].vtx1[2]));
-            (*model)->vtx.push_back(Vector3(buffer[i].vtx2[0],buffer[i].vtx2[1],buffer[i].vtx2[2]));
-            (*model)->vtx.push_back(Vector3(buffer[i].vtx3[0],buffer[i].vtx3[1],buffer[i].vtx3[2]));
+            (*model)->vtx.push_back(Vector3(buffer[i].vtx1[0],
+                                    buffer[i].vtx1[1],buffer[i].vtx1[2]));
+            (*model)->vtx.push_back(Vector3(buffer[i].vtx2[0],
+                                    buffer[i].vtx2[1],buffer[i].vtx2[2]));
+            (*model)->vtx.push_back(Vector3(buffer[i].vtx3[0],
+                                    buffer[i].vtx3[1],buffer[i].vtx3[2]));
 
-            (*model)->nor.push_back(Vector3(buffer[i].normal[0],buffer[i].normal[1],buffer[i].normal[2]));
+            (*model)->nor.push_back(Vector3(buffer[i].normal[0],
+                                    buffer[i].normal[1],buffer[i].normal[2]));
 
-            xCoords = Vector3(buffer[i].vtx1[0],buffer[i].vtx2[0],buffer[i].vtx3[0]);
-            yCoords = Vector3(buffer[i].vtx1[1],buffer[i].vtx2[1],buffer[i].vtx3[1]);
-            zCoords = Vector3(buffer[i].vtx1[2],buffer[i].vtx2[2],buffer[i].vtx3[2]);
+            xCoords = Vector3(buffer[i].vtx1[0],buffer[i].vtx2[0],
+                              buffer[i].vtx3[0]);
+            yCoords = Vector3(buffer[i].vtx1[1],buffer[i].vtx2[1],
+                              buffer[i].vtx3[1]);
+            zCoords = Vector3(buffer[i].vtx1[2],buffer[i].vtx2[2],
+                              buffer[i].vtx3[2]);
 
-            if((xCoords.minCoeff() < (*model)->minX) || i==0) (*model)->minX = xCoords.minCoeff();
-            if((xCoords.maxCoeff() > (*model)->maxX) || i==0) (*model)->maxX = xCoords.maxCoeff();
+            if((xCoords.minCoeff() < (*model)->minX) || i==0)
+               (*model)->minX = xCoords.minCoeff();
+            if((xCoords.maxCoeff() > (*model)->maxX) || i==0)
+               (*model)->maxX = xCoords.maxCoeff();
 
-            if((yCoords.minCoeff() < (*model)->minY) || i==0) (*model)->minY = yCoords.minCoeff();
-            if((yCoords.maxCoeff() > (*model)->maxY) || i==0) (*model)->maxY = yCoords.maxCoeff();
+            if((yCoords.minCoeff() < (*model)->minY) || i==0)
+               (*model)->minY = yCoords.minCoeff();
+            if((yCoords.maxCoeff() > (*model)->maxY) || i==0)
+               (*model)->maxY = yCoords.maxCoeff();
 
-            if((zCoords.minCoeff() < (*model)->minZ) || i==0) (*model)->minZ = zCoords.minCoeff();
-            if((zCoords.maxCoeff() > (*model)->maxZ) || i==0) (*model)->maxZ = zCoords.maxCoeff();
+            if((zCoords.minCoeff() < (*model)->minZ) || i==0)
+               (*model)->minZ = zCoords.minCoeff();
+            if((zCoords.maxCoeff() > (*model)->maxZ) || i==0)
+               (*model)->maxZ = zCoords.maxCoeff();
 
             for(int j=0; j<3; j++) tri.v[j] = i*3+j;
             tri.n = i;
@@ -89,5 +92,5 @@ int loadSTLModel(Model **model, std::string path){
       }
    }
 
-   return 1;
+   return 0;
 }
